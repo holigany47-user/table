d90838614d1b3976321a52a339b5e0d83e39480f
holigany47-user <holigany47@gmail.com>
2025-11-12 18:32:15 +0300

    Delete bot/handlers/__init__.py

--- bot/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from .start import setup_handlers as setup_start_handlers
-from .files import setup_handlers as setup_files_handlers
-
-def setup_all_handlers(application):
-    """Настройка всех обработчиков"""
-    setup_start_handlers(application)
-    setup_files_handlers(application)
